c2fae4c985b1d6eaae54c4ce6e1449a55775fcbd
anjara3389 <anjara3389@hotmail.com>
2017-10-31 17:52:14 -0500

    r

--- Category/ListCategory.php
+++ Category/ListCategory.php
@@ -6,6 +6,7 @@ $categories = $categ->select();
 <html>
     <head>
         <script type="text/javascript" src="./Category.js"></script>
+        <script type = "text/javascript"  src = "../jQuery/jquery-2.1.3.min.js"></script>
     </head>
     <body>
         <a href="./FrmCategory.php">Nuevo</a>
@@ -19,10 +20,19 @@ $categories = $categ->select();
                     $category = $categories[$i];
                     $id = $category->id;
                     $name = $category->name;
-                    echo "<tr><td>$name</td><td><a href='./FrmCategory.php?id=$id'>Editar</a></td><td><a id='del' name='del' href='./Category.php?id=$id&func=del'>Eliminar</a></td></tr>";
+                    echo "<tr><td>$name</td><td><a href='./FrmCategory.php?id=$id'>Editar</a></td><td><a id='del' name='del' href='./Category.php?id=$id&func=del' onclick='javascript:deleteConfirmation($id);'>Eliminar</a></td></tr>";
                 }
             }
             ?>
         </table>
     </body>
+    <script type="text/javascript">
+        function deleteConfirmation(idD) {
+            if (confirm("Desea eliminar la categoría?")) {
+                Category cat = new Category();
+                cat.delete(idD)
+            }
+        }
+    </script>
 </html>
+
